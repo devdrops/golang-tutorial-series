f47406c4c3e6306bd9d48ab0c95ef8c6f1e4b9a1
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2019-05-14 12:26:50 -0300

    Finished types! :D

--- go/src/types/types.go
+++ go/src/types/types.go
@@ -9,6 +9,7 @@ func main() {
 	// Golang has the following types:
 
 	// Boolean
+	//
 	a, b := true, false
 	fmt.Println("A is", a, "and B is", b)
 	c := a && b
@@ -17,6 +18,7 @@ func main() {
 	fmt.Println("D is", d, "as the result of a || b")
 
 	// Signed integers
+	//
 	// int is the default type for integers. When in doubt, this should be used.
 	i := int(100)
 	fmt.Printf("i is a variable of type %T and size of %d\n", i, unsafe.Sizeof(i))
@@ -38,6 +40,7 @@ func main() {
 	fmt.Printf("i64 is a variable of type %T and size of %d\n", i64, unsafe.Sizeof(i64))
 
 	// Unsigned integers
+	//
 	// ui8 is int8, a representation of 8 bit unsigned integer. uint8 goes from 0
 	// to 255.
 	ui8 := uint8(254)
@@ -56,6 +59,7 @@ func main() {
 	fmt.Printf("ui64 is a variable of type %T and size of %d\n", ui64, unsafe.Sizeof(ui64))
 
 	// Floating points
+	//
 	// Notice that there is no float as default type.
 	// float32 uses 32 bits.
 	f32 := float32(5.67)
@@ -67,6 +71,62 @@ func main() {
 	fmt.Printf("f64 is a variable of type %T and size of %d\n", f64, unsafe.Sizeof(f64))
 
 	// Complex types
-	comp64 := complex(3, 4)
-	comp
+	//
+	// complex64 is for complex numbers with float32 real and imaginary parts.
+	comp64 := complex(3, float32(4.16))
+	fmt.Printf("comp64, %d, is too complex: %T, size of %d\n", comp64, comp64, unsafe.Sizeof(comp64))
+	// complex128 is for complex numbers with float64 real and imaginary parts.
+	comp128 := complex(3, float64(4.16))
+	fmt.Printf("comp128, %d, is waaay too complex: %T, size of %d\n", comp128, comp128, unsafe.Sizeof(comp128))
+	// We can also create these complex numbers using the shorthand syntax.
+	complexAF := 6 + 7i
+	fmt.Printf("complexAF, %d, is of course complex too: %T, size of %d\n", complexAF, complexAF, unsafe.Sizeof(complexAF))
+	// Moar examples of complex numbers:
+	c1 := complex(5, 7)
+	c2 := 8 + 27i
+	cadd := c1 + c2
+	fmt.Println("The sum of c1 and c2 is", cadd)
+	cmul := c1 * c2
+	fmt.Println("The product of c1 and c2 is", cmul)
+
+	// Other numeric types
+	//
+	// We have byte and rune, which works as alias for other numeric types:
+	// byte is an alias for uint8
+	byteMe := byte(100)
+	fmt.Printf("byteMe is of type %T and size %d\n", byteMe, unsafe.Sizeof(byteMe))
+	// While rune is an alias for int32
+	oldRune := rune(100)
+	fmt.Printf("oldRune is of type %T and size %d\n", oldRune, unsafe.Sizeof(oldRune))
+
+	// Strings
+	//
+	// As strings in C are collections of chars, strings in Go are collections of
+	// bytes. WUT?
+	// We can use + to concatenate strings:
+	firstName := "José"
+	lastName := "Couves"
+	fullName := firstName + " das " + lastName
+	fmt.Println("O Lendário", fullName)
+
+	// Type Conversion
+	//
+	// As we noticed, Go is strictly typed. So, it means that there is no automatic
+	// type conversion:
+	// qtde is an int
+	qtde := 2
+	// and price is float64
+	price := 15.3
+	// So we'll never know how much we have to pay! The code below will raise a
+	// `invalid operation: qtde * price (mismatched types int and float64)`.
+	//totalCost := qtde * price
+	// In order to pay the bill, both qtde and price must be of the same type.
+	// Let's use some conversion power here:
+	totalCost := qtde * int(price)
+	fmt.Printf("The total cost, %d, is of type %T\n", totalCost, totalCost)
+	// Did you noticed the failure? Now we have a bug in our code, fly you fools!!
+	// The same conversion should be used when declaring the type explicitly:
+	eita := 1
+	var fooBarBaz float64 = float64(eita)
+	fmt.Printf("fooBarBaz is of type %T and value %d\n", fooBarBaz, fooBarBaz)
 }
